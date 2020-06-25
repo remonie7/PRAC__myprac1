dcc2d1cb00bba8bcc4bb80da35b16dd3a9926423
remonie7 <remonie7>
2020-06-25 17:11:42 +0900

    initial commit

---
+++ src/main/java/net/myprac1/Myprac1Application.java
@@ -0,0 +1,13 @@
+package net.myprac1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myprac1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myprac1Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/myprac1/Myprac1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.myprac1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myprac1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
