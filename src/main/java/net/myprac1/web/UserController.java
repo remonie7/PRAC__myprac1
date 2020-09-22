2abc75f937b697390100790a9ed12332a4082b41
remonie7 <remonie7@gmail.com>
2020-09-22 11:20:41 +0900

    제거했던 슬래시 다시 입력

--- src/main/java/net/myprac1/web/UserController.java
+++ src/main/java/net/myprac1/web/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
 	
 	@GetMapping("/form")
 	public String form() {
-		return "user/form";
+		return "/user/form";
 	}
 	
 	@PostMapping("")
@@ -32,14 +32,14 @@ public class UserController {
 	@GetMapping("")
 	public String userList(Model model) {
 		model.addAttribute("users", userRepository.findAll());
-		return "user/list";
+		return "/user/list";
 	}
 	
 	@GetMapping("/{id}/form")
 	public String updateForm(@PathVariable Long id, Model model) {
 		User user = userRepository.findById(id).get();  
 		model.addAttribute("user", user);
-		return "user/updateForm";
+		return "/user/updateForm";
 	}
 	
 	@PostMapping("/{id}")
