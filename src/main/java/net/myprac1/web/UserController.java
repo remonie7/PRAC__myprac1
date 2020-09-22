0c79f2c7d3c222e5142a1d31e2f3701deb9d2866
remonie7 <remonie7@gmail.com>
2020-09-22 11:31:06 +0900

    경로수정 6

--- src/main/java/net/myprac1/web/UserController.java
+++ src/main/java/net/myprac1/web/UserController.java
@@ -13,36 +13,36 @@ import net.myprac1.domain.User;
 import net.myprac1.domain.UserRepository;
 
 @Controller
-//@RequestMapping("/users")
+@RequestMapping("/users")
 public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@GetMapping("/users/form")
+	@GetMapping("/form")
 	public String form() {
-		return "/user/form";
+		return "user/form";
 	}
 	
-	@PostMapping("/users")
+	@PostMapping("")
 	public String userCreate(User user) {
 		userRepository.save(user);
 		return "redirect:/users";
 	}
 	
-	@GetMapping("/users")
+	@GetMapping("")
 	public String userList(Model model) {
 		model.addAttribute("users", userRepository.findAll());
-		return "/user/list";
+		return "user/list";
 	}
 	
-	@GetMapping("/users/{id}/form")
+	@GetMapping("/{id}/form")
 	public String updateForm(@PathVariable Long id, Model model) {
 		User user = userRepository.findById(id).get();  
 		model.addAttribute("user", user);
-		return "/user/updateForm";
+		return "user/updateForm";
 	}
 	
-	@PostMapping("/users/{id}")
+	@PostMapping("/{id}")
 	public String update(@PathVariable Long id, User updateUser) {
 		User user = userRepository.findById(id).get();  
 		user.update(updateUser);
