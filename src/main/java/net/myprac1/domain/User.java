700080d52ed85fd539026432ff7a42e40737312e
remonie7 <remonie7@gmail.com>
2020-09-22 10:20:11 +0900

    html 템플릿 추가, h2데이터베이스, 자바 객체와 테이블 매핑, 회원가입, 개인정보 수정

--- src/main/java/net/myprac1/domain/User.java
+++ src/main/java/net/myprac1/domain/User.java
@@ -1,7 +1,19 @@
-package net.myprac1.web;
+package net.myprac1.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(nullable=false, length=20)
 	private String userId;
+	
 	private String userPassword;
 	private String userName;
 	private String userEmail;
@@ -21,11 +33,20 @@ public class User {
 	public void setUserEmail(String userEmail) {
 		this.userEmail = userEmail;
 	}
+	
+	public void update(User updateUser) {
+		// TODO Auto-generated method stub
+		this.userPassword = updateUser.userPassword;
+		this.userName = updateUser.userName;
+		this.userEmail = updateUser.userEmail;
+	}
 
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", userPassword=" + userPassword + ", userName=" + userName + ", userEmail="
 				+ userEmail + "]";
 	}
+
+
 
 }
--- src/main/java/net/myprac1/web/UserController.java
+++ src/main/java/net/myprac1/web/UserController.java
@@ -1,28 +1,53 @@
 package net.myprac1.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.myprac1.domain.User;
+import net.myprac1.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-	private List<User> users = new ArrayList<User>();
+	@Autowired
+	private UserRepository userRepository;
 	
-	@PostMapping("/create")
-	public String create(User user) {
-//		System.out.println("id : "+userId+", pw : "+userPassword+", name : "+userName+", email : "+userEmail);
-		System.out.println("user : "+ user);
-		users.add(user);
-		return "redirect:/list";
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
 	}
 	
-	@GetMapping("/list")
-	public String list(Model model) {
-		model.addAttribute("users", users);
-		return "list";
+	@PostMapping("")
+	public String userCreate(User user) {
+		userRepository.save(user);
+		return "redirect:/users";
 	}
+	
+	@GetMapping("")
+	public String userList(Model model) {
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		User user = userRepository.findById(id).get();  
+		model.addAttribute("user", user);
+		return "/user/updateForm";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User updateUser) {
+		User user = userRepository.findById(id).get();  
+		user.update(updateUser);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+	
 }
