acd6ea8a62f5bde2ffeff4ac470f1120cbfcdf3c
remonie7 <remonie7@gmail.com>
2020-09-29 10:02:56 +0900

    외부 tomcat에 war 배포하도록 설정 추가

---
+++ src/main/java/net/myprac1/MyWebInitializer.java
@@ -0,0 +1,12 @@
+package net.myprac1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class MyWebInitializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(Myprac1Application.class);
+	}
+}
